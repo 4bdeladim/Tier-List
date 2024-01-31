52e084de56b0790406fc2d9ce2946232569d374d
4bdeladim <abdeladim.frontend@gmail.com>
2024-01-31 14:18:36 +0100

    Fix link

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full justify-center items-center">
-			<Link href="/create">
+			<Link href="/create/new">
 				<Button>
 					Create new TierList
 				</Button>
